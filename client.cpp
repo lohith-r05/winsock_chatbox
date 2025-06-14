7b43bacffdec716bc507822b823bf184a4bd548e
Lohtih <lohithrangaraj@gmail.com>
2025-06-14 17:37:10 +0530

    Update client.cpp
    -> for multi-user without database

--- client.cpp
+++ client.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <winsock2.h>
-
+#include <thread>
 using namespace std;
 
 class Client {
     WSADATA wsa;
     SOCKET clientSocket;
     sockaddr_in serverAddr;
+    string name;
 
 public:
     Client(const char* serverIP, int port) {
@@ -34,10 +35,41 @@ public:
         }
 
         cout << "Connected to server.\n";
+        cout << "Enter your name: ";
+        getline(cin, name);
     }
 
-    void sendMessage(const string& msg) {
-        send(clientSocket, msg.c_str(), msg.size() + 1, 0);
+    // Function to receive messages from server
+    void receiveLoop() {
+        char buffer[1024];
+        while (true) {
+            ZeroMemory(buffer, sizeof(buffer));
+            int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
+            if (bytesReceived <= 0) {
+                cout << "Disconnected from server.\n";
+                break;
+            }
+            cout << buffer << endl;
+        }
+    }
+
+    // Function to send messages to server
+    void sendLoop() {
+        string msg;
+        while (true) {
+            getline(cin, msg);
+            string fullMessage = name + ": " + msg;
+            send(clientSocket, fullMessage.c_str(), fullMessage.size() + 1, 0);
+        }
+    }
+
+    void startChat() {
+        thread receiveThread(&Client::receiveLoop, this);
+        thread sendThread(&Client::sendLoop, this);
+
+        // Wait for both threads to finish (they typically won't unless server disconnects)
+        receiveThread.join();
+        sendThread.join();
     }
 
     ~Client() {
@@ -47,13 +79,7 @@ public:
 };
 
 int main() {
-    Client client("192.168.35.108", 8080); // Connect to localhost
-
-    string message;
-    cout << "Enter the content to send to server: ";
-    getline(cin, message);
-
-    client.sendMessage(message);
-
+    Client client("192.168.35.108", 8080); // Replace with your server IP if needed
+    client.startChat();
     return 0;
 }
